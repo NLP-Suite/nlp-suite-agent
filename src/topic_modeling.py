14ae200fbeed8ce21590d8282bb58d8a328a8101
sherry <sherry.rui1@gmail.com>
2024-09-19 17:58:13 -0400

    updated topic modeling

--- src/topic_modeling.py
+++ src/topic_modeling.py
@@ -1,7 +1,6 @@
-import GUI_IO_util
 import topic_modeling_bert_util
 import topic_modeling_mallet_util
-import topic_modeling_gensim_util
+#import topic_modeling_gensim_util
 import IO_internet_util
 import reminders_util
 import IO_files_util
@@ -16,54 +15,77 @@ parser.add_argument("-o", "--outputFilename", help="Directs the output to a file
 args = parser.parse_args()
 """
 
-
 def run_topic_modeling(inputDir, outputDir, openOutputFiles, chartPackage, dataTransformation, num_topics,
-        BERT_var,
-        split_docs_var,
-        MALLET_var,
-        optimize_intervals_var,
-        Gensim_var,
-        remove_stopwords_var, lemmatize_var, nounsOnly_var, Gensim_MALLET_var):
+                       BERT_var,
+                       split_docs_var,
+                       MALLET_var,
+                       optimize_intervals_var,
+                       Gensim_var,
+                       remove_stopwords_var, lemmatize_var, nounsOnly_var, Gensim_MALLET_var):
 
     print(inputDir, outputDir, openOutputFiles, chartPackage, dataTransformation, num_topics,
-        BERT_var,
-        split_docs_var,
-        MALLET_var,
-        optimize_intervals_var,
-        Gensim_var,
-        remove_stopwords_var, lemmatize_var, nounsOnly_var, Gensim_MALLET_var)
+          BERT_var,
+          split_docs_var,
+          MALLET_var,
+          optimize_intervals_var,
+          Gensim_var,
+          remove_stopwords_var, lemmatize_var, nounsOnly_var, Gensim_MALLET_var)
     
     scriptName = "topic_modeling.py"
+
+    # Initialize filesToOpen list
+    filesToOpen = []
+
     
-    if not BERT_var and not MALLET_var and not Gensim_var:
-        print(title='Warning', message='There are no options selected.\n\nPlease, select one of the available options (MALLET or Gensim) and try again.')
+    labels = []
+    if BERT_var:
+        labels.append('BERTopic')
+    if MALLET_var:
+        labels.append('MALLET')
+    if Gensim_var:
+        labels.append('Gensim')
+
+    if not labels:
+        print('Warning: No options selected. Please select at least one of the available options (BERTopic, MALLET, or Gensim) and try again.')
         return
 
-    
-    if BERT_var:
-        label = 'BERTopic'
-    if MALLET_var:
-        label = 'MALLET'
-    if Gensim_var:
-        label = 'Gensim'
+    label = '-'.join(labels)
 
+    # Check internet availability
     if not IO_internet_util.check_internet_availability_warning(label + ' Topic Modeling'):
         return
 
-    if num_topics==20:
+    if num_topics == 20:
         reminders_util.checkReminder(scriptName, reminders_util.title_options_topic_modelling_number_of_topics,
                                      reminders_util.message_topic_modelling_number_of_topics, True)
 
-    # create a subdirectory of the output directory
-    outputDir = IO_files_util.make_output_subdirectory('', inputDir, outputDir, label='TM-'+label,
-                                                       silent=True)
+    # Create a subdirectory of the output directory
+    outputDir = IO_files_util.make_output_subdirectory('', inputDir, outputDir, label='TM-' + label, silent=True)
     if outputDir == '':
         return
+
+    # Run BERTopic
     if BERT_var:
-        topic_modeling_bert_util.run_BERTopic(inputDir, outputDir, openOutputFiles, split_docs_var)
+        
+        bert_files = topic_modeling_bert_util.run_BERTopic(inputDir, outputDir, split_docs_var)
+        # if bert_files:
+        #     filesToOpen.extend(bert_files)
+    
+    # Run MALLET
     if MALLET_var:
-        topic_modeling_mallet_util.run_MALLET(inputDir, outputDir, openOutputFiles, chartPackage, dataTransformation,
-                                                     optimize_intervals_var, num_topics)
-    if Gensim_var:
-        topic_modeling_gensim_util.run_Gensim(None, inputDir, outputDir, GUI_IO_util.config_filename, num_topics, #GUI_UTIL.window <- None
-                                          remove_stopwords_var, lemmatize_var, nounsOnly_var, Gensim_MALLET_var, openOutputFiles, chartPackage, dataTransformation)
+
+        mallet_files = topic_modeling_mallet_util.run_MALLET(inputDir, outputDir, chartPackage, dataTransformation,
+                                                             optimize_intervals_var, num_topics)
+        # if mallet_files:
+        #     filesToOpen.extend(mallet_files)
+    
+    # Run Gensim
+    #if Gensim_var:
+        # gensim_files = topic_modeling_gensim_util.run_Gensim(inputDir, outputDir, num_topics,
+        #                                                      remove_stopwords_var, lemmatize_var, nounsOnly_var,
+        #                                                      Gensim_MALLET_var, chartPackage, dataTransformation)
+        # if gensim_files:
+        #     filesToOpen.extend(gensim_files)
+    
+
+    return #filesToOpen
